191a5299d0d0ee3d3093b0b5ad83412c2d3fb4bb
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-27 02:03:29 -0500

    Cleanup
    Removed CDR and CAR practice. Created test list to match assignment.
    Added comments. Ran Test Cases

--- MaalikProgram.rkt
+++ MaalikProgram.rkt
@@ -1,21 +1,16 @@
-(define test-list (list 1 2 3))
+(define test-list0 '())
+(define test-list1 (list 1 2 3)) ;defining list for test case
+(define test-list2 '(a (b c) ((d e) f) g))
+(define test-list3 '(1 (2 3) (4 (a (b (c d))))))
 
-(CAR '(A B C))
+(define (reverse-general l) ;defining function with one parameter, l
+  (if (null? l) ;if the list is null
+      '() ;function will return empty set
+      (append (reverse-general (cdr l)) (list (car l))) ;else function will take the first element and add it to the tail of the list 
+      ) ;the use of reverse-general w/in itself makes it a recursive function
+  )
 
-(CDR '(A B C))
-
-(CDR '((A B) C D))
-
-(CONS (CAR '(A B C))(CDR '(A B C)))
-
-(CONS (CAR '(1 2 3))(CDR '(A B C)))
-
-(CAR test-list)
-
-(CDR test-list)
-
-(define (reverse-general l) (if (null? l) '() (append (reverse-general (cdr l)) (list (car l)))))
-
-(reverse-general test-list)
-
-
+(reverse-general test-list0) ;call recursive function reverse general
+(reverse-general test-list1)
+(reverse-general test-list2)
+(reverse-general test-list3)
