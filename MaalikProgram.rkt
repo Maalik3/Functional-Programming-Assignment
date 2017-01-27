a37e9726be73356578026f08e3d3ac3746a15556
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-27 12:45:40 -0500

    Final Part 1
    Removal of Test Cases

--- MaalikProgram.rkt
+++ MaalikProgram.rkt
@@ -1,8 +1,3 @@
-(define test-list0 '())
-(define test-list1 (list 1 2 3)) ;defining list for test case
-(define test-list2 '(a (b c) ((d e) f) g))
-(define test-list3 '(1 (2 3) (4 (a (b (c d))))))
-
 (define (reverse-general l) ;defining function with one parameter, l
   (if (null? l) ;if the list is null
       '() ;function will return empty set
@@ -10,7 +5,3 @@
       ) ;the use of reverse-general w/in itself makes it a recursive function
   )
 
-(reverse-general test-list0) ;call recursive function reverse general
-(reverse-general test-list1)
-(reverse-general test-list2)
-(reverse-general test-list3)
