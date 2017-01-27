45a710592a9004a8c24ba663df3b4826f92c528c
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-27 01:16:16 -0500

    Working w/ list Functions
    Working with list functions to obtain an understanding of CAR, CONS, CDR

--- MaalikProgram.rkt
+++ MaalikProgram.rkt
@@ -1,26 +1,12 @@
-(+ 3 4)
-
-(+ 5 (* 2 2 ))
-
-(define foo 3)
-
-foo
-
-(* foo 4)
-
-(define (square x) (* x x))
-
-(square 2)
-
-(+ (square 2) (square 2))
-
-(define (absolute x) (if (< x 0) (-x) x))
-
-(list 1 2 3)
-
-(length (list 1 4 2 9 8))
-
 (define test-list (list 1 2 3 4 5))
 
+(CAR '(A B C))
 
-      
+(CDR '(A B C))
+
+(CDR '((A B) C D))
+
+(CONS (CAR '(A B C))(CDR '(A B C)))
+
+(CONS (CAR '(1 2 3))(CDR '(A B C)))
+
