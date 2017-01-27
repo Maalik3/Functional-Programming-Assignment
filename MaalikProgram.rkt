d42a449c0ffe6b10b74c84ffc9fc8a3b140a4914
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-27 00:51:10 -0500

    Introduction to List

--- MaalikProgram.rkt
+++ MaalikProgram.rkt
@@ -1,7 +1,5 @@
 (+ 3 4)
 
-(* 3 4)
-
 (+ 5 (* 2 2 ))
 
 (define foo 3)
@@ -18,3 +16,11 @@ foo
 
 (define (absolute x) (if (< x 0) (-x) x))
 
+(list 1 2 3)
+
+(length (list 1 4 2 9 8))
+
+(define test-list (list 1 2 3 4 5))
+
+
+      
