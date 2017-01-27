fa486949212ca72cd805ecf8e3060c74ec0349c1
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-27 13:10:15 -0500

    First Attempt at Recursive Algorithm
    My first attempt to recursively sum the list is returning an error
    “mcdr: contract violation
      expected: mpair?
      given: 1”

--- MaalikProgram.rkt
+++ MaalikProgram.rkt
@@ -5,3 +5,14 @@
       ) ;the use of reverse-general w/in itself makes it a recursive function
   )
 
+(define test-list1 (list 1 2 3))
+(cdr test-list1)
+(car test-list1)
+
+(define (sum-up-numbers-simple l) ;defining function with one parameter, l
+  (if (null? l) ;if the list is null
+      '() 
+      (+ (cdr l) (sum-up-numbers-simple (car l)))
+      )
+  )
+(sum-up-numbers-simple test-list1)
