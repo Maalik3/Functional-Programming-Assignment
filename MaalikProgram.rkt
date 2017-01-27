31106a8cc496d6f26b7815e1a2404e739e157f8e
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-27 00:39:01 -0500

    Basics of Scheme
    Practicing basic concepts of scheme: arithmetic & functions

---
+++ MaalikProgram.rkt
@@ -0,0 +1,20 @@
+(+ 3 4)
+
+(* 3 4)
+
+(+ 5 (* 2 2 ))
+
+(define foo 3)
+
+foo
+
+(* foo 4)
+
+(define (square x) (* x x))
+
+(square 2)
+
+(+ (square 2) (square 2))
+
+(define (absolute x) (if (< x 0) (-x) x))
+
