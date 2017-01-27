3d14ba87e97ca55da4f3e5efc477ed80802467f8
Maalik <maalik.hornbuckle@bison.howard.edu>
2017-01-27 14:28:50 -0500

    Adding functionality
    Changed if else to handle non numeral items, as well as adding
    conditional statement if list is null

--- MaalikProgram.rkt
+++ MaalikProgram.rkt
@@ -6,11 +6,14 @@
   )
 
 (define (sum-up-numbers-simple l) ;defining function wit hone parameter l
-  (if
-    (null? l) ;if the list is null
-    0 ;will return 0 
-     (+ (car l) (sum-up-numbers-simple (cdr l))) ;removed first item of the list and added to second item by using CDR to retrieve list without first item, and recursively calling sum function to retrieve the first item 
-  )
+  (cond ((null? L) 0) ;conditional statement that says if the list is null, return 0
+        
+     ((number? (car L)) ;Runs check to be certain that the first character is a number
+      
+      (+ (car l) (sum-up-numbers-simple (cdr l))) ;removed first item of the list and added to second item by using CDR to retrieve list without first item, and recursively calling sum function to retrieve the first item
+      
+     (else (+ 0 (sum-up-numbers-simple (cdr L)))) ;if not a number, it will add 0 to the total
+    )
 )
 
 
